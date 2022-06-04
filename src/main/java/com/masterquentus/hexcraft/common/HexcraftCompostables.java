390d0a29f2b5fdc342c95ca7110a95e133b95a26
Dalto <DaltonRPratt@gmail.com>
2022-06-03 19:51:41 -0500

    Added Blood Fluid

--- src/main/java/com/masterquentus/hexcraft/common/HexcraftCompostables.java
+++ src/main/java/com/masterquentus/hexcraft/common/HexcraftCompostables.java
@@ -6,25 +6,25 @@ import net.minecraft.world.level.ItemLike;
 import net.minecraft.world.level.block.ComposterBlock;
 
 public class HexcraftCompostables {
-    public static void compostibleBlocks(float chance, ItemLike item) {
+    public static void compostableBlocks(float chance, ItemLike item) {
         ComposterBlock.COMPOSTABLES.put(item.asItem(), chance);
     }
 
-    public static void compostablesHex() {
-        HexCraft.LOGGER.debug("Hexcraft: Adding Compostible Blocks...");
+    public static void compostableHex() {
+        HexCraft.LOGGER.debug("Hexcraft: Adding Compostable Blocks...");
 
         //Leaves
-        compostibleBlocks(0.3f, HexcraftBlocks.EBONY_LEAVES.get());
-        compostibleBlocks(0.3f, HexcraftBlocks.BLOOD_OAK_LEAVES.get());
+        compostableBlocks(0.3f, HexcraftBlocks.EBONY_LEAVES.get());
+        compostableBlocks(0.3f, HexcraftBlocks.BLOOD_OAK_LEAVES.get());
 
 
         //Saplings
-        compostibleBlocks(0.3f, HexcraftBlocks.EBONY_SAPLING.get());
-        compostibleBlocks(0.3f, HexcraftBlocks.BLOOD_OAK_SAPLING.get());
+        compostableBlocks(0.3f, HexcraftBlocks.EBONY_SAPLING.get());
+        compostableBlocks(0.3f, HexcraftBlocks.BLOOD_OAK_SAPLING.get());
 
 
         //Flowers
-        compostibleBlocks(0.65f, HexcraftBlocks.BLOODY_ROSE.get());
+        compostableBlocks(0.65f, HexcraftBlocks.BLOODY_ROSE.get());
 
 
         //Mushroom Blocks
@@ -32,10 +32,10 @@ public class HexcraftCompostables {
 
 
         //Plants
-        compostibleBlocks(0.65f, HexcraftBlocks.EBONY_LEAVES_PILE.get());
-        compostibleBlocks(0.65f, HexcraftBlocks.BLOOD_OAK_LEAVES_PILE.get());
+        compostableBlocks(0.65f, HexcraftBlocks.EBONY_LEAVES_PILE.get());
+        compostableBlocks(0.65f, HexcraftBlocks.BLOOD_OAK_LEAVES_PILE.get());
 
-        HexCraft.LOGGER.info("Hexcraft: Compostible Blocks Added!");
+        HexCraft.LOGGER.info("Hexcraft: Compostable Blocks Added!");
 
         }
 
